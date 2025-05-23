a6fcfe075502be26844599b15b5cd00acd5ebd99
deirdreidah <idahdeirdre@gmail.com>
2025-05-23 12:37:38 +0300

    Y1S1

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from app.extensions import db,migrate
-from app.controllers.course import course
-from app.controllers.program import program
-from app.controllers.student import student
+from app.controllers.course.course_controller  import course
+from app.controllers.program.program_controller import program
+from app.controllers.student.student_controller import student
 
 
 #application factory function
